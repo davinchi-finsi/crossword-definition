982a018e5aa946f9ed9c18ed03b177fe9d1ec9aa
Javier Pérez Ruiz <javier.perez@davinchi.es>
2018-02-02 10:30:21 +0100

    Added distribution Added es2015 bundle for fuse

--- fuse.js
+++ fuse.js
@@ -1,42 +1,45 @@
 const {
     FuseBox,
-    QuantumPlugin,
-    UglifyESPlugin
+    QuantumPlugin
 } = require("fuse-box");
 const Sparky = require("fuse-box/sparky");
 const extend = require("extend");
 const path = require("path");
 Sparky.context(class {
-    getConfig() {
-        this.fuse= FuseBox.init({
+    getConfig(dist,es6) {
+        return FuseBox.init({
             homeDir: "src",
-            output: "dist/$name.js",
-            target: "browser@es6",
+            output: `${dist}/$name.js`,
+            target: `browser@${es6 ? 'es2015' : 'es5'}`,
             cache:false,
             sourceMaps:!this.isProduction,
             globals: { 'default': '*' },
             plugins: [
                 this.isProduction && QuantumPlugin({
                     containedAPI: true,
-                    ensureES5: true,
+                    ensureES5: !es6,
                     uglify: this.isProduction,
                     bakeApiIntoBundle: "crossword-definition",
                     noConflictApi:true
                 })
             ]
         });
-        return this.fuse;
     }
-    createBundle() {
-        const app = this.fuse.bundle("crossword-definition");
+    createBundle(fuse) {
+        const app = fuse.bundle("crossword-definition");
         if (!this.isProduction) {
             app.watch();
         }
         app.instructions(">index.ts");
         return app;
     }
-    getTsConfig(){
-        return require(path.resolve("src","tsconfig.json"));
+    createES6Bundle(fuse) {
+        const app = fuse.bundle("crossword-definition");
+        if (!this.isProduction) {
+            app.watch();
+        }
+        app.instructions(">index.ts");
+        return app;
     }
 });
 
@@ -46,14 +49,23 @@ Sparky.task("clean", () => {
 });
 
 Sparky.task("default", ["clean"], context => {
-    const fuse = context.getConfig();
-    context.createBundle();
+    const fuse = context.getConfig("dist",false);
+    context.createBundle(fuse);
     fuse.run();
 });
 
-Sparky.task("dist", ["clean"],async context=>{
+Sparky.task("distES5",async context=>{
     context.isProduction = true;
-    const fuse = context.getConfig();
-    context.createBundle();
+    const fuse = context.getConfig("dist",false);
+    context.createBundle(fuse);
     await fuse.run();
+});
+Sparky.task("distES2015",async context=>{
+    context.isProduction = true;
+    const fuse = context.getConfig("esm2015",true);
+    context.createBundle(fuse);
+    await fuse.run();
+});
+
+Sparky.task("dist", ["clean","distES5","distES2015"],async context=>{
 });
